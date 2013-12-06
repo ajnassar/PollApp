bbaee00b9fc6975153008165d77190a433c25b4b
enquea <student@appacademy.io>
2013-12-06 13:18:49 -0800

    Setup initial table creation migrations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PollApp::Application.config.secret_token = '63d8cfa0fb2f87bf3925a77e70198eb4b8a87f997b58ce9c6e41839ac7fd5d1a3878a29aa2f9fcf0fc500bcc42186d70468c7a9df54cd0c105018b33495dda22'
